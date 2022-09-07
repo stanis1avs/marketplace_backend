47efa0d42a40ad1907c1c56e17ed29c3aa41b170
Stanislav <S.tanislav-Z.abrodin@yandex.ru>
2022-09-07 23:04:27 +0400

    init

--- server.js
+++ server.js
@@ -1,81 +1,118 @@
 const http = require('http');
+const fs = require('fs');
 const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('koa2-cors');
 const koaBody = require('koa-body');
 
-const app = new Koa();
-app.use(cors());
-app.use(koaBody({ json: true }));
+const categories = JSON.parse(fs.readFileSync('./data/categories.json'));
+const items = JSON.parse(fs.readFileSync('./data/products.json'));
+const topSaleIds = [66, 65, 73];
+const moreCount = 6;
 
-let nextId = 1;
-const services = [
-    { id: nextId++, name: 'Замена стекла', price: 21000, content: 'Стекло оригинал от Apple'},
-    { id: nextId++, name: 'Замена дисплея', price: 25000, content: 'Дисплей оригинал от Foxconn'},
-    { id: nextId++, name: 'Замена аккумулятора', price: 4000, content: 'Новый на 4000 mAh'},
-    { id: nextId++, name: 'Замена микрофона', price: 2500, content: 'Оригинальный от Apple'},
-];
+const itemBasicMapper = item => ({
+    id: item.id,
+    category: item.category,
+    title: item.title,
+    price: item.price,
+    images: item.images,
+});
 
-const router = new Router();
+const randomNumber = (start, stop) => {
+    return Math.floor(Math.random() * (stop - start + 1)) + start;
+}
 
-function fortune(ctx, body = null, status = 200) {
+const fortune = (ctx, body = null, status = 200) => {
+    // Uncomment for delay
+    const delay = randomNumber(1, 10) * 1000;
+    //const delay = 0;
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (Math.random() > 0.25) {
-                ctx.response.status = status;
-                ctx.response.body = body;
-                resolve();
+            // Uncomment for error generation
+            if (Math.random() > 0.8) {
+                reject(new Error('Something bad happened'));
                 return;
             }
 
-            reject(new Error('Something bad happened'));
-        }, 3 * 1000);
+            ctx.response.status = status;
+            ctx.response.body = body;
+            resolve();
+        }, delay);
     })
 }
 
-router.get('/api/services', async (ctx, next) => {
-    const body = services.map(o => ({id: o.id, name: o.name, price: o.price}))
-    return fortune(ctx, body);
-});
-router.get('/api/services/:id', async (ctx, next) => {
-    const id = Number(ctx.params.id);
-    const index = services.findIndex(o => o.id === id);
-    if (index === -1) {
-        const status = 404;
-        return fortune(ctx, null, status);
-    }
-    const body = services[index];
-    return fortune(ctx, body);
-});
-router.post('/api/services', async (ctx, next) => {
-    const id = ctx.request.body.id;
-    if (id !== 0) {
-        const index = services.findIndex(o => o.id === id);
-        if (index === -1) {
-            const status = 404;
-            return fortune(ctx, null, status);
-        }
-        services[index] = ctx.request.body;
-        return fortune(ctx, null, 204);
-    }
-    
-    services.push({ ...ctx.request.body, id: nextId++ });
-    const status = 204;
-    return fortune(ctx, null, status);
-});
-router.delete('/api/services/:id', async (ctx, next) => {
-    const id = Number(ctx.params.id);
-    const index = services.findIndex(o => o.id === id);
-    if (index === -1) {
-        const status = 404;
-        return fortune(ctx, null, status);
-    }
-    services.splice(index, 1);
-    const status = 204;
-    return fortune(ctx, null, status);
+const app = new Koa();
+app.use(cors());
+app.use(koaBody({
+    json: true
+}));
+
+const router = new Router();
+
+router.get('/api/top-sales', async (ctx, next) => {
+    return fortune(ctx, items.filter(o => topSaleIds.includes(o.id)).map(itemBasicMapper));
 });
 
-app.use(router.routes());
+router.get('/api/categories', async (ctx, next) => {
+    return fortune(ctx, categories);
+});
+
+router.get('/api/items', async (ctx, next) => {
+    const { query } = ctx.request;
+
+    const categoryId = query.categoryId === undefined ? 0 : Number(query.categoryId);
+    const offset = query.offset === undefined ? 0 : Number(query.offset);
+    const q = query.q === undefined ? '' : query.q.trim().toLowerCase();
+
+    const filtered = items
+        .filter(o => categoryId === 0 || o.category === categoryId)
+        .filter(o => o.title.toLowerCase().includes(q) || o.color.toLowerCase() === q)
+        .slice(offset, offset + moreCount)
+        .map(itemBasicMapper);
+
+    return fortune(ctx, filtered);
+});
+
+router.get('/api/items/:id', async (ctx, next) => {
+    const id = Number(ctx.params.id);
+    const item = items.find(o => o.id === id);
+    if (item === undefined) {
+        return fortune(ctx, 'Not found', 404);
+    }
+
+    return fortune(ctx, item);
+});
+
+router.post('/api/order', async (ctx, next) => {
+    const { owner: { phone, address }, items } = ctx.request.body;
+    if (typeof phone !== 'string') {
+        return fortune(ctx, 'Bad Request: Phone', 400);
+    }
+    if (typeof address !== 'string') {
+        return fortune(ctx, 'Bad Request: Address', 400);
+    }
+    if (!Array.isArray(items)) {
+        return fortune(ctx, 'Bad Request: Items', 400);
+    }
+    if (!items.every(({ id, price, count }) => {
+        if (typeof id !== 'number' || id <= 0) {
+            return false;
+        }
+        if (typeof price !== 'number' || price <= 0) {
+            return false;
+        }
+        if (typeof count !== 'number' || count <= 0) {
+            return false;
+        }
+        return true;
+    })) {
+        return fortune(ctx, 'Bad Request', 400);
+    }
+
+    return fortune(ctx, null, 204);
+});
+
+app.use(router.routes())
 app.use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
